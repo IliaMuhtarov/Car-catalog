d10f0e418e07909d7bf1d485330e5ee22b5291aa
IliaMuhtarov <90609655+IliaMuhtarov@users.noreply.github.com>
2024-01-21 23:50:20 +0300

    Feat (ui): refactor car form

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,13 +1,25 @@
+import { cars as carsData} from './cars.data.js'
+import CarItem from "./car-item/CarItem.jsx";
+import CreateCarForm from "./create-car-form/CreateCarForm.jsx";
+import {useState} from "react";
+
 function Home() {
+const [cars, setCars] = useState(carsData)
+    console.log(cars)
+
     return (
         <div>
             <h1>Cars catalog</h1>
+                <CreateCarForm setCars = {setCars} />
             <div>
-                <h2>Car 1</h2>
-                <p>$100 000</p>
-                <button>Read more</button>
+                {cars.length ? (
+                    cars.map(car => ( <CarItem key={car.id} car = {car}/> ))
+                ) : (
+                    <p>No cars now!</p>
+                )}
             </div>
         </div>
     )
 }
+
 export default Home
---
+++ src/components/screens/home/cars.data.js
@@ -0,0 +1,20 @@
+export const cars = [
+    {
+        id: 1,
+        name:"Toyota",
+        price: 10000,
+        image: 'http://localhost:4200/assets/images/car1.jpg'
+    },
+    {
+        id: 2,
+        name:  "Lada",
+        price: 250000,
+        image: 'http://localhost:4200/assets/images/car2.webp'
+    },
+    {
+        id: 3,
+        name:"Toyota444",
+        price: 30000,
+        image: 'http://localhost:4200/assets/images/car1.jpg'
+    }
+]
